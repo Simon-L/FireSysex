9353bf58d653750eb4da3289314ce8f5160b79e3
Simon-L <lumis@xulepth.fr>
2022-11-04 22:11:38 +0100

    Add native version with shared UI code

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # DPF + ImGui template plugin project
 
 This repository contains an example audio plugin project using DPF and ImGui.
+
+## Optional native version with common UI
+
+```
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+```
+
+Native is built here: `build/imgui-demo-native`
+
--- src/CommonUI.cpp
+++ src/CommonUI.cpp
@@ -1,21 +1,39 @@
-/*
- * ImGui plugin example
- * Copyright (C) 2021 Jean Pierre Cimalando <jp-dev@inbox.ru>
- * Copyright (C) 2021-2022 Filipe Coelho <falktx@falktx.com>
- * SPDX-License-Identifier: ISC
- */
-
+#include "Application.hpp"
+#include "DearImGui.hpp"
 #include "DistrhoUI.hpp"
+
+#ifdef ISPLUGIN
+
+// ------- Plugin
 #include "ResizeHandle.hpp"
-
 START_NAMESPACE_DISTRHO
+typedef UI _UI;
+// -------
 
-// --------------------------------------------------------------------------------------------------------------------
+#else
 
-class ImGuiPluginUI : public UI
+// ------- Native
+#include <cstdio>
+typedef ImGuiStandaloneWindow _UI;
+// -------
+
+#endif 
+
+#ifdef ISNATIVE
+void editParameter(int index, bool active) {
+    printf("%s\n", (active ? "Editing" : "Not editing"));
+}
+void setParameterValue(int index, float value) {
+    printf("Set value %f\n", value);
+}
+#endif
+
+class ImGuiPluginUI : public _UI
 {
     float fGain = 0.0f;
+#ifdef ISPLUGIN
     ResizeHandle fResizeHandle;
+#endif
 
     // ----------------------------------------------------------------------------------------------------------------
 
@@ -24,22 +42,30 @@ public:
       UI class constructor.
       The UI should be initialized to a default state that matches the plugin side.
     */
+#ifdef ISPLUGIN
     ImGuiPluginUI()
         : UI(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT),
           fResizeHandle(this)
+#else
+    ImGuiPluginUI(Application& app)
+        : ImGuiStandaloneWindow(app)
+#endif
     {
         setGeometryConstraints(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT, true);
 
+        #ifdef ISPLUGIN
         // hide handle if UI is resizable
         if (isResizable())
             fResizeHandle.hide();
+        #endif
     }
 
 protected:
     // ----------------------------------------------------------------------------------------------------------------
     // DSP/Plugin Callbacks
 
-   /**
+    #ifdef ISPLUGIN
+    /**
       A parameter has changed on the plugin side.@n
       This is called by the host to inform the UI about parameter changes.
     */
@@ -50,6 +76,7 @@ protected:
         fGain = value;
         repaint();
     }
+    #endif
 
     // ----------------------------------------------------------------------------------------------------------------
     // Widget Callbacks
@@ -66,7 +93,13 @@ protected:
         ImGui::SetNextWindowPos(ImVec2(margin, margin));
         ImGui::SetNextWindowSize(ImVec2(width - 2 * margin, height - 2 * margin));
 
-        if (ImGui::Begin("Simple gain", nullptr, ImGuiWindowFlags_NoResize))
+        #ifdef ISPLUGIN
+        const char* title = "Simple gain - plugin";
+        #else
+        const char* title = "Simple gain - native";
+        #endif
+
+        if (ImGui::Begin(title, nullptr, ImGuiWindowFlags_NoResize))
         {
             static char aboutText[256] = "This is a demo plugin made with ImGui.\n";
             ImGui::InputTextMultiline("About", aboutText, sizeof(aboutText));
@@ -90,6 +123,7 @@ protected:
     DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ImGuiPluginUI)
 };
 
+#ifdef ISPLUGIN
 // --------------------------------------------------------------------------------------------------------------------
 
 UI* createUI()
@@ -100,3 +134,14 @@ UI* createUI()
 // --------------------------------------------------------------------------------------------------------------------
 
 END_NAMESPACE_DISTRHO
+#else
+int main(int, char**)
+{
+    Application app;
+    ImGuiPluginUI win(app);
+    win.show();
+    app.exec();
+    printf("Hey?\n");
+    return 0;
+}
+#endif
